6b64da461029c834f5cc646cd870425150659c84
sanat630 <sanatabdunazarov@gmail.com>
2023-03-02 19:03:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const searchValue = document.getElementById('search-value')
+
+const form = document.getElementById("search--form")
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(searchValue.value);
+    
+  });
+  
