b64b1848d027a80f3a3167758c02ed2964468e86
Sanat630 <sanatabdunazarov@gmail.com>
2023-03-05 06:39:41 +0500

    fix: card

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const searchValue = document.getElementById('search-value')
 
 const form = document.getElementById("search--form")
+
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(searchValue.value);
     
   });
-  
